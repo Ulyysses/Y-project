550121f21ea3c14e35f363919416c3e91487588e
Ulyana Sanina <sanina.ulyana111@gmail.com>
2022-08-17 21:21:25 +0400

    Minor Fixes

--- src/components/app-header/AppHeader.js
+++ src/components/app-header/AppHeader.js
@@ -9,37 +9,35 @@ import css from "./index.module.css";
 const AppHeader = () => {
   return (
     <header className={css.header}>
-      <nav className={css.header_nav}>
+      <nav className={`${css.header_nav} ${css.container}`}>
         <ul className={css.nav_ul}>
-          <div className={css.two_li}>
-            <li>
-              <a href="#" className={css.nav_link}>
-                <BurgerIcon type="secondary" />
-                <span
-                  className={`${css.nav_link_text} text text_type_main-default ml-2`}
-                >
-                  Конструктор
-                </span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className={css.nav_link}>
-                <ListIcon type="secondary" />
-                <span
-                  className={`${css.nav_link_text} text text_type_main-default ml-2`}
-                >
-                  Лента заказов
-                </span>
-              </a>
-            </li>
-          </div>
+          <li>
+            <a href="#" className={`${css.nav_link} pl-5 pr-5 mr-2`}>
+              <BurgerIcon type="secondary" />
+              <span
+                className={`${css.nav_link_text} text text_type_main-default ml-2`}
+              >
+                Конструктор
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="#" className={css.nav_link}>
+              <ListIcon type="secondary" />
+              <span
+                className={`${css.nav_link_text} text text_type_main-default ml-2`}
+              >
+                Лента заказов
+              </span>
+            </a>
+          </li>
           <li className={css.nav_logo}>
             <a href="#">
               <Logo />
             </a>
           </li>
-          <li>
-            <a href="#" className={css.nav_link}>
+          <li className={css.nav_profileIcon}>
+            <a href="#" className={`${css.nav_link} mr-5`}>
               <ProfileIcon type="secondary" />
               <span
                 className={`${css.nav_link_text} text text_type_main-default ml-2`}
