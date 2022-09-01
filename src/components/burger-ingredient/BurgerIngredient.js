a3c5426a89d1dee35d680dc025a7e8e180d11f75
Ulyana Sanina <sanina.ulyana111@gmail.com>
2022-09-01 12:25:34 +0400

    Add props & typography digits

--- src/components/burger-ingredient/BurgerIngredient.js
+++ src/components/burger-ingredient/BurgerIngredient.js
@@ -1,34 +1,27 @@
-import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  CurrencyIcon,
+  Counter,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import classNames from "classnames";
 import css from "./index.module.scss";
 
-const BurgerIngredient = () => {
-  const data = {
-    _id: "60666c42cc7b410027a1a9b1",
-    name: "Краторная булка N-200i",
-    type: "bun",
-    proteins: 80,
-    fat: 24,
-    carbohydrates: 53,
-    calories: 420,
-    price: 1255,
-    image: "https://code.s3.yandex.net/react/code/bun-02.png",
-    image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
-    image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
-    __v: 0,
-  };
-
+const BurgerIngredient = ({ name, price, image }) => {
   return (
     <div className={css.item}>
-      <img src={data.image} className={css.img} width="240" height="120"></img>
+      <img
+        src={image}
+        className={css.img}
+        width="240"
+        height="120"
+        alt={name}
+      ></img>
+      <Counter count={1} size="default" />
       <div className={css.price}>
-        <p className={classNames("text text_type_main-default", css.p_price)}>
-          {data.price}
-        </p>
+        <p className="text text_type_digits-default">{price}</p>
         <CurrencyIcon type="primary" />
       </div>
-      <p className={classNames("text text_type_main-small", css.title)}>
-        {data.name}
+      <p className={classNames("text text_type_main-default", css.title)}>
+        {name}
       </p>
     </div>
   );
