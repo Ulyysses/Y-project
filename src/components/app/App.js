4311e5adb0b1cbcd7f5b65f46701595f76bc9a7b
Ulyana Sanina <sanina.ulyana111@gmail.com>
2022-10-21 14:21:57 +0400

    change classname

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,7 +8,7 @@ const App = () => {
     <>
       <AppHeader />
       <main className="container">
-        <h1 className={classNames("text text_type_main-large mt-10 mb-5")}>
+        <h1 className="text text_type_main-large mt-10 mb-5">
           Соберите бургер
         </h1>
         <div className="small_container">
