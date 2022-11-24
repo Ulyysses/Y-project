fa428dd6373c4c4d40b640b75ce4cb2355048bb9
Ulyana Sanina <sanina.ulyana111@gmail.com>
2022-11-24 19:57:16 +0400

    add modal overlay

---
+++ src/components/modal-overlay/ModalOverlay.js
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+import css from "./index.module.scss";
+import PropTypes from "prop-types";
+
+const ModalOverlay = ({ children, open, onClose }) => {
+  const modalRootElement = document.querySelector("#modal");
+
+  useEffect(() => {
+    const closeWithEscape = (e) => {
+      if (e.code === "Escape") {
+        onClose(e);
+      }
+    };
+
+    document.body.style.overflow = "hidden";
+    document.addEventListener("keydown", closeWithEscape);
+
+    return () => {
+      console.log("test");
+
+      document.body.style.overflow = "auto";
+      document.removeEventListener("keydown", closeWithEscape);
+    };
+  });
+
+  if (open) {
+    return createPortal(
+      <div className={css.overlay} onClick={onClose}>
+        {children}
+      </div>,
+      modalRootElement
+    );
+  }
+
+  return null;
+};
+
+ModalOverlay.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+export default ModalOverlay;
