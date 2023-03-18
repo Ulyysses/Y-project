7caa6adc996e5ef6c4877e0fc15d47625239de4f
Ulyana Sanina <sanina.ulyana111@gmail.com>
2023-03-18 22:41:02 +0400

    storage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./services/reducers/rootReducer";
+import { Provider } from "react-redux";
 import App from "./components/app/App";
 import "./index.scss";
 import "./layout.scss";
 
+const store = configureStore(rootReducer);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
