34e1ababa92e505d46b25010c06fef69f0b3f258
Ulyana Sanina <sanina.ulyana111@gmail.com>
2023-03-20 17:35:20 +0400

    enhancer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
 import { rootReducer } from "./services/reducers/rootReducer";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import App from "./components/app/App";
 import "./index.scss";
 import "./layout.scss";
 
-const store = configureStore(rootReducer);
+const store = configureStore(rootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
