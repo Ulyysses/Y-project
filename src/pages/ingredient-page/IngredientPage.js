33098bafad4af647665f9d20e1a93c193f2f351a
Ulyana Sanina <sanina.ulyana111@gmail.com>
2023-06-15 13:30:32 +0400

    add IngredientPage

---
+++ src/pages/ingredient-page/IngredientPage.js
@@ -0,0 +1,24 @@
+import IngredientDetails from "../../components/ingredient-details/IngredientDetails";
+import css from "./index.module.scss";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+const IngredientPage = () => {
+  const { id } = useParams();
+
+  const dataIngredients = useSelector(
+    (state) => state.ingredients.dataIngredients
+  );
+
+  const currentIngredient = dataIngredients.find((ingredient) => {
+    return ingredient._id === id;
+  });
+
+  return (
+    <div className={css.ingredient_wrapper}>
+      <IngredientDetails data={currentIngredient} />
+    </div>
+  );
+};
+
+export default IngredientPage;
