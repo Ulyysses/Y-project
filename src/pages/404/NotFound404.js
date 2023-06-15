1f261aa42c31a6723b7fbc10b91db41ca611d789
Ulyana Sanina <sanina.ulyana111@gmail.com>
2023-06-15 13:34:54 +0400

    add page 404

---
+++ src/pages/404/NotFound404.js
@@ -0,0 +1,11 @@
+import css from "./index.module.scss";
+
+const NotFound404 = () => {
+  return (
+    <div className={css.notfound_container}>
+      <p className="text text_type_main-large">404</p>
+    </div>
+  );
+};
+
+export default NotFound404;
