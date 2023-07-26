593567287b4c7feb53b12329365d1fabe220be17
Ulyana Sanina <sanina.ulyana111@gmail.com>
2023-07-26 16:47:10 +0400

    websocket

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./services/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
@@ -8,8 +8,55 @@ import App from "./components/app/App";
 import "./index.scss";
 import "./layout.scss";
 import { BrowserRouter } from "react-router-dom";
+import { socketMiddleware } from "./services/middleware/socketMiddleware";
+import { getCookie } from "./utils/cookie";
+import {
+  wsConnectionSuccess,
+  wsConnectionError,
+  wsConnectionClosed,
+  wsGetOrders,
+  wsConnectionStart,
+} from "./services/orders";
+import {
+  allConnectionSuccess,
+  allConnectionError,
+  allConnectionClosed,
+  allGetOrders,
+  allConnectionStart,
+} from "./services/ordersAll.js";
 
-const store = configureStore(rootReducer, applyMiddleware(thunk));
+const wsOrdersActions = [
+  wsConnectionSuccess,
+  wsConnectionError,
+  wsConnectionClosed,
+  wsGetOrders,
+  wsConnectionStart,
+];
+
+const allOrdersAllActions = [
+  allConnectionSuccess,
+  allConnectionError,
+  allConnectionClosed,
+  allGetOrders,
+  allConnectionStart,
+];
+
+const store = configureStore({
+  ...rootReducer,
+  middleware: [
+    thunk,
+    socketMiddleware(
+      `wss://norma.nomoreparties.space/orders?token=${getCookie(
+        "accessToken"
+      )}`,
+      wsOrdersActions
+    ),
+    socketMiddleware(
+      `wss://norma.nomoreparties.space/orders/all`,
+      allOrdersAllActions
+    ),
+  ],
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/services/reducers/rootReducer.js
+++ src/services/reducers/rootReducer.js
@@ -1,9 +1,13 @@
 import ingredientsSlice from "../ingredients";
 import modalSlice from "../modal";
+import ordersSlice from "../orders";
+import allOrdersSlice from "../ordersAll";
 
 export const rootReducer = {
   reducer: {
     ingredients: ingredientsSlice,
     modal: modalSlice,
+    orders: ordersSlice,
+    allOrders: allOrdersSlice,
   },
 };
